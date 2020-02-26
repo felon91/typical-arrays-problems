49288b5c77882bb1373b37d38dc2744617f40f69
felon91 <felon_91@mail.ru>
2020-02-26 20:59:01 +0300

    feat: implement function typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) return 0;
+    return array.reduce((prev, item) => {
+        return (prev > item) ? item : prev;
+    });
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) return 0;
+    return array.reduce((prev, item) => {
+        return (prev > item) ? prev : item;
+    });
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) return 0;
+    return array.reduce((prev, item) => {
+        return prev += item;
+    }) / array.length;
 }
